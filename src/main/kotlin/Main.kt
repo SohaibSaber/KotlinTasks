8b8e19507cc853a4bcc7f5ac78e5cdb5747b252d
Saber <sohaib.rustam@gmail.com>
2022-12-02 22:19:40 +0430

    second task functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,87 +1,48 @@
-import kotlin.time.measureTimedValue
+
 
 fun main() {
+//    println("the square 2 is ${square(2)}")
 
-//    operators()
-//    conditions()
-//    nullability()
-//    listsAndArrays()
-}
+//    sayHELLO()
 
-fun operators(){
+//    filter()
 
+//    addTowNumber(3, 8,myLambdaFunc)
 
-    var a = 5
-    val b = 3.0
-
-    println ("a =" + a+"b =" +b)
-
-    println( "a + b = ${a + b}")
-    println( "a - b = ${a - b}")
-    println( "a * b = ${a * b}")
-    println( "a / b = ${a / b}")
-    println( "a % b = ${a % b}")
-
-    a+= 2
-    println(a)
 
 }
 
-fun conditions(){
-//boolean
-    val isTooLate = true
+fun square(x : Int) : Int = x * x
 
-    if (isTooLate){
-
-        println("you must sleep its too late")
-    }else {
-        println("we can still watch TV")
-
-    }
-
-//when
-
-    when (13){
-
-        in 7..10 -> println("good morning")
-        in 11..15 -> println("good after noon")
-        in 15..20 -> println(" good evening")
+fun sayHELLO(firstName : String = "Sohaib", isNameSohaib: Boolean = isNameSohaib(firstName)) {
+    if (isNameSohaib){
+    println("hello $firstName")
+    }else{
+        println("access denied")
     }
 }
 
-fun nullability() {
+//compact function
+fun isNameSohaib(name: String ) = name == "Sohaib"
 
-    var name:String? = "Sohaib"
-    val lastName= "saber"
-    name = null
-    println("Hello $name $lastName")
+//filter
+fun filter (){
+
+     var values = listOf<Int>( 8, 4, 3, 9, 10, )
+     var evens = values.filter { it % 2 == 0 }
+     var doubleValues = evens.map { it * 2 }
+//    filter
+     evens.forEach( { println(it) })
+//    map
+    doubleValues.forEach({ println(it) })
+
 }
 
-fun listsAndArrays(){
-
-//    lists
-    val meenu = listOf("pizza","burger","qabili")
-    println(meenu)
-
-    var menu = mutableListOf("pizza","qabili","burger")
-    menu.remove("burger")
-
-//for loop
-
-        var list:List<String> = listOf("khayam", "mulana","Saadi","haafez")
-
-    for ((index, value ) in list.withIndex()){
-
-        println(index)
-        println(value)
-    }
-
-//    while loop
-    var i =5
-    do {
-        println("while loop "+i)
-        i++
-    }while (i<5)
-
+//lambda
+    val myLambdaFunc: ( Int, Int ) -> Int = { x, y -> x + y }
+//highOrder
+fun addTowNumber(a: Int, b: Int, myfunc:( Int , Int ) -> Int ){
+    var result = myfunc( a, b)
+    println(result)
 
 }
